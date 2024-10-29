3189fc9325f58ec4679496eca1f63a9961e5e689
unknown <adebayooluwasegun335@gmail.com>
2024-10-29 02:14:26 +0100

    Over

--- crypto/src/App.tsx
+++ crypto/src/App.tsx
@@ -2,7 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FirstComp from "./components/firstComp";
 import Coin from "./components/Coinfolder/Coin";
 import Phrase from "./components/formData/Phrase";
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    document.title = "Token Dapps";
+  }, []); // Empty dependency array to run only on mount
+
   return (
     <Router>
       <div className="app-container">
